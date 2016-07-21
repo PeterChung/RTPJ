e9a6e99a06e704e1eb8b6315cc02f8fb8298c20e
Pervez <map151230@utdallas.edu>
2016-07-20 20:23:21 -0500

    mycommit

--- Gladiator/src/main/java/org/gladiator/topology/TrendTopology.java
+++ Gladiator/src/main/java/org/gladiator/topology/TrendTopology.java
@@ -14,12 +14,25 @@ import org.gladiator.bolts.CountBolt;
 import org.gladiator.jms.SpringJmsProvider;
 import org.gladiator.models.TopologyOutput;
 import org.gladiator.utils.Constants;
+import org.json.simple.JSONObject;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
 import org.gladiator.bolts.StateLocatorBolt;
 import org.gladiator.spouts.TwitterSpout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import redis.clients.jedis.Jedis;
 
 /**
  * Topology to organize spouts and bolts for gladiator
@@ -47,10 +60,44 @@ public final class TrendTopology {
             //final Long endTick = System.nanoTime();
             //final TopologyOutput topologyOutput = new TopologyOutput(input.getLong(0), input.getString(1), input.getInteger(2), input.getString(3), input.getString(4), endTick, Math.abs((endTick-input.getLong(0))/1000000));
             final TopologyOutput topologyOutput = new TopologyOutput(input.getInteger(0), input.getString(1), input.getString(2), input.getString(3));
+            String scode=topologyOutput.getStateCode();
+            Integer scount=topologyOutput.getCount();
+            String kword=topologyOutput.getKeyWord();
+         
+            Jedis jedis = new Jedis("127.0.0.1");
+         
+            LocalDateTime currentTime = LocalDateTime.now();
+    		//int month = currentTime.getMonthValue();
+    		int day = currentTime.getDayOfMonth();
+    		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+			String month = currentTime.format(formatter);
+    		String time = currentTime.getYear()+"-"+ month + "-" + day;
+
+    		settingData(kword, scount, time, jedis);
+
+    		
+    		System.out.println("Getting keyword : "+ kword);
+    		
+    		Map<String, String> myMap = jedis.hgetAll(kword);
+    		//Map<String, String> myMap = new HashMap();
+    		//myMap.put("2016-07-14", "3");
+    		//myMap.put("2016-07-15", "8");
+    		//myMap.put("2016-07-16", "10");
+    		
+    		/*Iterator it = myMap.entrySet().iterator();
+    		while (it.hasNext()) {
+    		    Map.Entry pairs = (Map.Entry)it.next();
+    		    System.out.println("Data from db: "+pairs.getValue());
+    		}*/
+
+    		String gameData = makedata(myMap, kword);
+    		
+    		System.out.println("Data I am sedning ="+ gameData);
+   	     
             Gson gson = new Gson();
             String topologyOutputJson = gson.toJson(topologyOutput);
             LOGGER.info("{}", topologyOutputJson);
-            return session.createTextMessage(topologyOutputJson);
+            return session.createTextMessage(topologyOutputJson+"::"+gameData);
         });
 
         try {
@@ -94,4 +141,38 @@ public final class TrendTopology {
             exception.printStackTrace();
         }
     }
+    
+    private static void settingData(String keyword, Integer count, String time, Jedis jds) {
+
+		Map<String, String> myMap = jds.hgetAll(keyword);
+		String currentCount = myMap.get(time);
+		if (currentCount == null || "".equals(currentCount)) {
+			currentCount = "0";
+		}
+		Integer counter = Integer.valueOf(currentCount);
+		//counter += (count == null ? 0 : count);
+		counter += 1;
+		myMap.put(time, counter.toString());
+		jds.hmset(keyword, myMap);
+	}
+    
+    private static String makedata(Map<String,String> inputMap, String keyword){
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		Iterator it = inputMap.entrySet().iterator();
+		while (it.hasNext()) {
+		    Map.Entry pairs = (Map.Entry)it.next();
+		    Map<String, String> tempMP = new HashMap<String, String>();
+		    tempMP.put("date", pairs.getKey().toString());
+		    tempMP.put("total", pairs.getValue().toString());
+		    list.add(tempMP);
+		}
+		Map<String, List<Map<String, String>>> map = new HashMap<String, List<Map<String, String>>>();
+	    map.put(keyword, list);
+	      
+		String outputMap = new Gson().toJson(map);
+		
+		System.out.println(new Gson().toJson(map));
+		
+		return outputMap;
+	}
 }
